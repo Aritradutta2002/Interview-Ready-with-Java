89166bd3c9ca99bf25f68340f26502e0ce5cab72
ARITRA DUTTA <aritradutta049@gmail.com>
2025-02-11 00:46:46 +0530

    modified and updated

---
+++ BINARY_TREES/LeetCode/HeightOfBinaryTree.java
@@ -0,0 +1,33 @@
+package BINARY_TREES.LeetCode;
+/*
+*   Author  : Aritra Dutta
+*   Created : Monday, 10.02.2025  01:01 am
+*/
+
+/*
+       # Height of a Binary Tree: --
+       The height of a binary tree is the number of edges on the longest path from the root node to a leaf node.
+       In other words, it's the distance from the root node to the deepest leaf node.
+
+        # Depth of a Binary Tree: --
+        The depth of a binary tree refers to the same concept—the number of edges from the root node to the leaf node.
+ */
+public class HeightOfBinaryTree{
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        root.left.left.left = new TreeNode(8);
+
+        System.out.println(height(root));
+    }
+
+    public static int height(TreeNode root){
+        if(root == null) return -1;
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+}
